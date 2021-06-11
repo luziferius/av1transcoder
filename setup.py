bee2e5a7b7fa623bcee8e17085af04f07d762693
Thomas Hess <thomas.hess@udo.edu>
2021-06-11 10:21:48 +0200

    Marked compatibility with Python 3.9

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Multimedia :: Video :: Conversion',
     ],
