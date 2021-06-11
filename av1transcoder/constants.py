95c2b04e75cd5be038bb760f79b72bb3833f647b
Thomas Hess <thomas.hess@udo.edu>
2021-06-11 10:23:25 +0200

    Release v0.4.2

--- av1transcoder/constants.py
+++ av1transcoder/constants.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 
 PROGRAMNAME = "av1transcoder"
